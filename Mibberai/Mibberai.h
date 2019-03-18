d22319c7f0c5fa4c39d8a4af6436b4f579706500
Mark Ratjens <mark@ratjens.com>
2019-03-18 14:55:16 +1100

    Initial Commit

---
+++ Mibberai/Mibberai.h
@@ -0,0 +1,19 @@
+//
+//  Mibberai.h
+//  Mibberai
+//
+//  Created by Mark Ratjens on 18/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mibberai.
+FOUNDATION_EXPORT double MibberaiVersionNumber;
+
+//! Project version string for Mibberai.
+FOUNDATION_EXPORT const unsigned char MibberaiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mibberai/PublicHeader.h>
+
+
